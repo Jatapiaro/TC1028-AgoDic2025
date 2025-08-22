f5f6d9e7dbdcc9fe8e0120294d83f216976de58c
Luis Castillo <a01826140@tec.mx>
2025-08-22 09:46:14 -0600

    Update ejercicio_4.py

--- src/ejercicios/clase_4/ejercicio_4.py
+++ src/ejercicios/clase_4/ejercicio_4.py
@@ -4,3 +4,5 @@ Si el número es menor a 0 deberá imprimir negativo
 Si es igual a 0 deberá imprimir cero
 Si es mayor a 0 deberá imprimir positivo
 """
+n = int(input("Ingrese su numero:\n"))
+print("negativo" if n < 0 else "cero" if n == 0 else "positivo")
