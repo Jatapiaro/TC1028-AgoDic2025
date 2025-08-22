0a75082f3f8a7d4eb2eb3afb83147fbc9ea88d51
Aldo P. <aldo2006.apm@gmail.com>
2025-08-22 09:59:41 -0600

    Update ejercicio_5.py
    Se agrega código

--- src/ejercicios/clase_4/ejercicio_5.py
+++ src/ejercicios/clase_4/ejercicio_5.py
@@ -8,3 +8,12 @@ Primavera (3, 4 y 5)
 Verano (6, 7, 8)
 Otoño (9, 10, 11)
 """
+mes = int(input("Ingresa el mes en numero: "))
+if mes == 1 or mes == 2 or mes == 12:
+  print("Invierno")
+elif mes >= 3 and mes <= 5:
+  print("Primavera")
+elif mes >= 6 and mes <= 8:
+  print("Verano")
+else:
+  print("Otoño")
