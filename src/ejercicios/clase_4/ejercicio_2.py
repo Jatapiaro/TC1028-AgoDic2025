05f34b8db57f2d4998d56b2a19243ffb5361263e
jayjay <167156089+jjnootfound@users.noreply.github.com>
2025-08-23 18:59:55 -0600

    feat: exercise two answers

--- src/ejercicios/clase_4/ejercicio_2.py
+++ src/ejercicios/clase_4/ejercicio_2.py
@@ -16,3 +16,21 @@ Ejemplo de ejecución:
 >>> 7.68
 8
 """
+import math
+
+def main():
+    calificacion = float(input("Ingresa la calificación con dos decimales(0 - 10): "))
+    if calificación == 0 or calificacion == 10:
+        print(calificacion)
+    else:
+        trunc_c = math.trunc(calificacion)
+        decimales = calificacion - trunc_c
+        if decimales == 0:
+            print(calificacion)
+        elif decimales >= 0.60:
+            print(math.ceil(calificacion))
+        else:
+            print(math.floor(calificacion))
+            
+if __name__ == "__main__":
+    main()
