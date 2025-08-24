58c6fc719b663b9d920b6f8a6bc3733d0cd6f2b0
jayjay <167156089+jjnootfound@users.noreply.github.com>
2025-08-23 19:09:59 -0600

    feat: exercise 3 answers

--- src/ejercicios/clase_4/ejercicio_3.py
+++ src/ejercicios/clase_4/ejercicio_3.py
@@ -3,3 +3,27 @@ Crea un programa que simule el lanzamiento de 2 monedas.
 Si ambas monedas caen en la misma cara, el programa deberá imprimir "Ganaste"
 De lo contrario el programa deberá imprimir "Perdiste".
 """
+
+import random
+
+
+def calculation(coin_1, coin_2, coin_3, coin_4):
+    return coin_1 + coin_2 + coin_3 + coin_4
+
+
+def main():
+    coin_1 = random.randint(0, 1)
+    coin_2 = random.randint(0, 1)
+    coin_3 = random.randint(0, 1)
+    coin_4 = random.randint(0, 1)
+
+    ans = calculation(coin_1, coin_2, coin_3, coin_4)
+
+    if ans == 0 or ans == 4:
+        print("Ganaste")
+    else:
+        print(f"Perdiste: {coin_1}, {coin_2}, {coin_3}, {coin_4}")
+
+
+if __name__ == "__main__":
+    main()
