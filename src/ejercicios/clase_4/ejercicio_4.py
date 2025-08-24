b184664389ce4de7983acebcfb044c48b3f495ee
jayjay <167156089+jjnootfound@users.noreply.github.com>
2025-08-23 19:17:55 -0600

    feat: exercise 4 answer

--- src/ejercicios/clase_4/ejercicio_4.py
+++ src/ejercicios/clase_4/ejercicio_4.py
@@ -4,3 +4,17 @@ Si el número es menor a 0 deberá imprimir negativo
 Si es igual a 0 deberá imprimir cero
 Si es mayor a 0 deberá imprimir positivo
 """
+def main():
+    num = int(input("Introduce un número: "))
+    
+    if num < 0: 
+        print("negativo")
+    elif num == 0: 
+        print("cero")
+    elif num > 0: 
+        print("positivo")
+    else:
+        print("entrada inválida")
+
+if __name__ == "__main__":
+    main()
