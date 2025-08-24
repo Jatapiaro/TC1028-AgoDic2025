ba2567b0143a7285e6a9410a3c5c558f0e6e4c09
jayjay <167156089+jjnootfound@users.noreply.github.com>
2025-08-23 19:22:36 -0600

    feat: ejercicio 5 answers

--- src/ejercicios/clase_4/ejercicio_5.py
+++ src/ejercicios/clase_4/ejercicio_5.py
@@ -2,9 +2,28 @@
 Crea un programa que pregunte al usuario un mes (1-12) y que imprima
 la estación correspondiente.
 
-Recuerda que:
+Recuerda que:s
 Invierno (1, 2 y 12)
 Primavera (3, 4 y 5)
 Verano (6, 7, 8)
 Otoño (9, 10, 11)
 """
+
+
+def main():
+    month = int(input("Introduce un mes en número: "))
+
+    if month in [1, 2, 12]:
+        print("invierno")
+    elif month in [3, 4, 5]:
+        print("primavera")
+    elif month in [6, 7, 8]:
+        print("verano")
+    elif month in [9, 10, 11]:
+        print("0toño")
+    else:
+        print("mes inválido")
+
+
+if __name__ == "__main__":
+    main()
