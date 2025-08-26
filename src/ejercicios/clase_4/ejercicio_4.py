037873d8ee782121d5975401af8ad85b20834bd2
Alex-Soph <A01825459@tec.mx>
2025-08-26 09:46:11 -0600

    Update ejercicio_4.py

--- src/ejercicios/clase_4/ejercicio_4.py
+++ src/ejercicios/clase_4/ejercicio_4.py
@@ -4,3 +4,5 @@ Si el número es menor a 0 deberá imprimir negativo
 Si es igual a 0 deberá imprimir cero
 Si es mayor a 0 deberá imprimir positivo
 """
+ejercicio_4 
+Alexhia Sophia Perez Escobar | A01825459
