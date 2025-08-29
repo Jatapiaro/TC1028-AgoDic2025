129a22bed2f07f39571ec9d424e43fdd606ee1f1
a01825096-sta <a01825096@tec.mx>
2025-08-28 22:13:57 -0600

    Update ejercicio_3_A01825096_Lulio Derek.py

--- src/ejercicios/clase_5/ejercicio_3.py
+++ src/ejercicios/clase_5/ejercicio_3.py
@@ -39,3 +39,40 @@ Ejemplos de ejecución del programa
 >>>m
 20
 """
+def suma(valor1: float, valor2:float) -> float:
+    return valor1 + valor2
+
+def resta(valor1: float, valor2:float) -> float:
+    return valor1 - valor2
+
+def multiplicacion(valor1: float, valor2:float) -> float:
+    return valor1 * valor2
+
+def division(valor1: float, valor2:float) -> float:
+    return valor1 / valor2
+
+def main():
+    n1 = float(input("Primer valor: "))
+    n2 = float(input("Segundo valor: "))
+    clave = input("Clave: ")
+    
+    if clave == "s":
+        res = suma(valor1 = n1, valor2 = n2)
+        print(f"La suma de los valores es : {suma(valor1 = n1, valor2 = n2)}")
+    
+    elif clave == "r":
+        res = resta(valor1 = n1, valor2 = n2)
+        print(f"La resta de los valores es : {resta(valor1 = n1, valor2 = n2)}")
+    
+    elif clave == "m":
+        res = multiplicacion(valor1 = n1, valor2 = n2)
+        print(f"La multiplicacion de los valores es : {multiplicacion(valor1 = n1, valor2 = n2)}")
+    
+    elif clave == "d":
+        res = division(valor1 = n1, valor2 = n2)
+        print(f"La division de los valores es : {division(valor1 = n1, valor2 = n2)}")
+    
+    else:
+        print("Opcion no valida")
+
+main()
