520ba59feae512a5cae03f4f98a8b74b56bf9f60
AldoAlexisYA <a01825644@tec.mx>
2025-09-11 19:50:31 -0600

    A01825644 ejercicio_5.py
    Aldo Alexis Yáñez Andreu

--- src/ejercicios/clase_9/ejercicio_5.py
+++ src/ejercicios/clase_9/ejercicio_5.py
@@ -1,3 +1,9 @@
 """
 Imprime un cuadrado de 5x5 usando el carácter asterisco (*).
 """
+def main():
+    x = int(input("Introduce de cuanto quieres que sea tu cuadrado"))
+    for i in range(0,x):
+        print("*"*x)
+
+main()
