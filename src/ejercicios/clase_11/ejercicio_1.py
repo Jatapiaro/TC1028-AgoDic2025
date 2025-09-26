a5cf1dcf2bbfba85e1778733bcd4ba6c63507a23
Jatapiaro <jacob.et.cetera@gmail.com>
2025-09-26 12:18:37 +0000

    Ejercicios 10

---
+++ src/ejercicios/clase_11/ejercicio_1.py
@@ -0,0 +1,9 @@
+"""
+Objetivo: Usar un bucle while para añadir elementos a una lista hasta que el
+usuario decida parar. La condición para detenerse es que el usuario escriba
+la palabra "salir".
+
+Instrucciones:
+Este programa te permitirá añadir tantas canciones como quieras a tu playlist.
+Cuando termines, solo escribe "salir".
+"""
---
+++ src/ejercicios/clase_11/ejercicio_2.py
@@ -0,0 +1,14 @@
+"""
+Objetivo: Crear un menú interactivo con un bucle while para manipular una lista
+de tareas de varias maneras.
+
+Instrucciones:
+Este es un administrador de tareas. Podrás ver tus tareas,
+añadir nuevas, y marcar las que ya hiciste.
+
+¡El programa no se detendrá hasta que elijas la opción de salir!
+1. Añadir tarea
+2. Ver tareas
+4. Seleccionar tarea al azar
+3. Marcar tarea como hecha
+"""
